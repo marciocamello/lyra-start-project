fbf1f771ddfde523a7e683259568d907ea210641
marciocamello <mac3designer@gmail.com>
2023-11-01 17:20:52 +0000

    Create a Lyra Blank Plugin for first tests

---
+++ Plugins/LyraGame/Source/LyraGame/LyraGame.Build.cs
@@ -0,0 +1,91 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class LyraGame : ModuleRules
+{
+	public LyraGame(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+			}
+		);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+			}
+		);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"CoreOnline",
+				"CoreUObject",
+				"ApplicationCore",
+				"Engine",
+				"PhysicsCore",
+				"GameplayTags",
+				"GameplayTasks",
+				"GameplayAbilities",
+				"AIModule",
+				"ModularGameplay",
+				"ModularGameplayActors",
+				"DataRegistry",
+				"ReplicationGraph",
+				"GameFeatures",
+				"SignificanceManager",
+				"Hotfix",
+				"CommonLoadingScreen",
+				"Niagara",
+				"AsyncMixin",
+				"ControlFlows",
+				"PropertyPath"
+			}
+		);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"InputCore",
+				"Slate",
+				"SlateCore",
+				"RenderCore",
+				"DeveloperSettings",
+				"EnhancedInput",
+				"NetCore",
+				"RHI",
+				"Projects",
+				"Gauntlet",
+				"UMG",
+				"CommonUI",
+				"CommonInput",
+				"GameSettings",
+				"CommonGame",
+				"CommonUser",
+				"GameSubtitles",
+				"GameplayMessageRuntime",
+				"AudioMixer",
+				"NetworkReplayStreaming",
+				"UIExtension",
+				"ClientPilot",
+				"AudioModulation",
+				"EngineSettings",
+				"DTLSHandlerComponent",
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+	}
+}
